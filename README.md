15a7a3652141cbc1cf343b2769055689aeb2fad4
Christophe Jolif <cjolif@gmail.com>
2014-06-26 17:20:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-deliteful-app [![Build Status](https://travis-ci.org/ibm-js/generator-deliteful-app.png?branch=master)](https://travis-ci.org/ibm-js/generator-deliteful-app)
+
+[Yeoman](http://yeoman.io) generator for applications leveraging [deliteful](https://github.com/ibm-js/deliteful) elements.
+
+## Status
+
+Latest release is 0.1.2
+
+## Issues
+
+Bugs and open issues are tracked in the
+[github issues tracker](https://github.com/ibm-js/generator-deliteful-app/issues).
+
+## Installation
+
+To install generator-deliteful-app from npm, run:
+
+```
+$ npm install -g generator-deliteful-app
+```
+
+Make a new directory and cd into it:
+
+```
+mkdir -p my-element && cd $_
+```
+
+Finally, initiate the generator to scaffold your application and follow the instructions:
+
+```
+$ yo deliteful-app
+```
+
+## Licensing & Contributions
+
+This project is distributed by the Dojo Foundation and licensed under the ["New" BSD License](./LICENSE).
+
+All contributions require a [Dojo Foundation CLA](http://dojofoundation.org/about/claForm).
---
+++ app/index.js
@@ -0,0 +1,75 @@
+/*global module, __dirname*/
+/*jshint globalstrict: false*/
+"use strict";
+/*jshint globalstrict: true*/
+var util = require("util");
+var path = require("path");
+var yeoman = require("yeoman-generator");
+var _ = require("underscore.string");
+
+
+var DelitefulAppGenerator = module.exports = function DelitefulAppGenerator(args, options, config) {
+	yeoman.generators.Base.apply(this, arguments);
+
+	this.on("end", function () {
+		var self = this;
+		this.installDependencies({
+			skipInstall: options["skip-install"],
+			callback: function () {
+				console.log("Dependencies have been installed, point your browser to index.html to run " +
+					"the skeleton sample application");
+			}
+		});
+	});
+
+	this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, "../package.json")));
+};
+
+util.inherits(DelitefulAppGenerator, yeoman.generators.Base);
+
+DelitefulAppGenerator.prototype.askFor = function askFor() {
+	var cb = this.async();
+
+	// have Yeoman greet the user.
+	console.log(this.yeoman);
+
+	// TODO:  which themes?, using dapp? build or unbuilt layer?, binding
+
+	this.prompt([
+		{
+			name: "package",
+			message: "What is the name of your deliteful application package?",
+			default: this.appname.indexOf(" ") !== -1 ? _.slugify(this.appname) : this.appname
+		},
+		{
+			type: "confirm",
+			name: "i18n",
+			message: "Will your delite application require string internationalization?",
+			default: false
+		},
+		{
+			type: "confirm",
+			name: "build",
+			message: "Do you want to use build or source version of deliteful package?",
+			default: false
+		}
+	], function (props) {
+		this.package = props.package;
+		this.i18n = props.i18n;
+		this.build = props.build;
+		cb();
+	}.bind(this));
+};
+
+DelitefulAppGenerator.prototype.generateApp = function app() {
+	// this.template("Gruntfile.js", "Gruntfile.js");
+	this.template("_package.json", "package.json");
+	this.template("_bower.json", "bower.json");
+	this.template("_index.html", "index.html");
+	this.copy("app.css", "app.css");
+
+};
+
+DelitefulAppGenerator.prototype.projectfiles = function projectfiles() {
+	this.copy("jshintrc", ".jshintrc");
+};
