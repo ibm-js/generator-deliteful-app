a24fef87dbb09717b818186ab4684f028af397ba
Christophe Jolif <cjolif@gmail.com>
2014-10-22 16:47:14 +0200

    adapt to deliteful-0.4.0-alpha.2

--- app/templates/app.js
+++ app/templates/app.js
@@ -1,7 +1,7 @@
 define([
 	"delite/register", "delite/theme!delite/themes/{{theme}}/global.css", "deliteful/ViewStack",
 	"deliteful/SidePane", "deliteful/LinearLayout", "deliteful/Button", "deliteful/StarRating",
-	"deliteful/ProgressBar", "deliteful/list/List", "requirejs-domready/domReady!"
+	"deliteful/ProgressBar", "deliteful/Store", "deliteful/list/List", "requirejs-domready/domReady!"
 ], function (register) {
 	register.parse();
 	document.body.style.display = "";
