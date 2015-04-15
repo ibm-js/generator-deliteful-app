c141c7ea08b83f93471171c7f0ac3ce86c817b4f
Christophe Jolif <christophe.jolif@fr.ibm.com>
2015-04-15 18:17:13 +0200

    update for deliteful 0.7

--- app/templates/app.js
+++ app/templates/app.js
@@ -1,10 +1,9 @@
 define([
-	"delite/register", "delite/theme!delite/themes/{{theme}}/global.css", "deliteful/ViewStack",
+	"delite/theme!delite/themes/{{theme}}/global.css", "deliteful/ViewStack",
 	"deliteful/SidePane", "deliteful/LinearLayout", "deliteful/Button", "deliteful/StarRating",
 	"deliteful/ProgressBar", "deliteful/Store", "deliteful/list/List",
 	"requirejs-dplugins/css!Font-Awesome/css/font-awesome.css", "requirejs-domready/domReady!"
-], function (register) {
-	register.parse();
+], function () {
 	document.body.style.display = "";
 	list.selectedItem = list.store.get("first");
 
